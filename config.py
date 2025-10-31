10ddbfea0dab41127915ff5458fcbb2b67d21819
s4760908 <s4760908@uq.edu.au>
2025-10-31 10:16:41 +1000

    Model hyperparameters

---
+++ config.py
@@ -0,0 +1,29 @@
+from dataclasses import dataclass
+
+
+@dataclass(frozen=True, slots=True)
+class VAEConfig:
+    learn_rate: float
+    num_channels: tuple[int, ...]
+    latent_dim: int
+    num_resnet_blocks: int
+    num_attention_heads: int
+    layer_norm_num_groups: int
+
+
+@dataclass(frozen=True, slots=True)
+class ImageInfo:
+    size: tuple[int, int]
+    depth: int
+
+
+@dataclass(frozen=True, slots=True)
+class DiffusionConfig:
+    learn_rate: float
+    noise_start: float
+    noise_end: float
+    denoise_steps: int
+    num_channels: tuple[int, ...]
+    num_resnet_blocks: int
+    num_attention_heads: int
+    layer_norm_num_groups: int
