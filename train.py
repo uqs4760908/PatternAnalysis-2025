8887244888ceedd1359cceea39e27005f7a1bfe3
s4760908 <s4760908@uq.edu.au>
2025-10-31 10:16:42 +1000

    LPIPS is not effective, remove it

--- train.py
+++ train.py
@@ -1,11 +1,9 @@
-import math
 from torch.utils.data import DataLoader
 from torch.utils.data.distributed import DistributedSampler
 from config import DiffusionConfig, EncoderDecoderConfig, ImageInfo, VAEConfig
 from dataset import AD_LABEL, NC_LABEL, NUM_CLASS, ANDIDataset, ImageListDataset
 from pathlib import Path
 from modules import VAE, DiffusionModel, DiffusionSampler
-from torchvision.transforms.v2 import functional as VF
 from torch import multiprocessing as mp
 from torch import distributed as dist
 from torch import GradScaler
@@ -17,7 +15,6 @@ from tempfile import NamedTemporaryFile
 from torch import nn, Tensor
 from torch.nn import functional as F
 from torch.optim import AdamW, Optimizer
-from torchmetrics.image.lpip import LearnedPerceptualImagePatchSimilarity
 from dataclasses import dataclass
 from io import StringIO
 import sys
@@ -611,23 +608,14 @@ class VAEController(ModelController):
                               weight_decay=VAE_CONFIG.weight_decay)
         self.scaler = PortableGradScaler()
         self.scheduler = CosineAnnealingLR(self.optimiser, self.num_epochs())
-        self.loss_vgg = LearnedPerceptualImagePatchSimilarity("vgg", normalize=True)
-        self.lpips_mse_anneal = 0
 
 
     def num_epochs(self) -> int:
         return 30
 
 
-    def dependent_models(self) -> typing.Sequence[nn.Module]:
-        return (self.loss_vgg,)
-
-
     def loss_fn(self, image: Tensor, generated: Tensor, mu: Tensor, logvar: Tensor) -> Tensor:
-        weight = math.exp(2 * (self.lpips_mse_anneal / self.num_epochs() - 2))
-        image = VF.grayscale_to_rgb(image)
-        generated = VF.grayscale_to_rgb(generated)
-        reconstruction = weight * self.loss_vgg(image, generated) + (1 - weight) * F.mse_loss(generated, image)
+        reconstruction = F.mse_loss(generated, image, reduction="none").sum(dim=[1, 2, 3]).mean()
         kld: Tensor = -0.5 * torch.sum(1 + logvar - mu.pow(2) - logvar.exp(), dim=(1, 2, 3)).mean()
 
         return reconstruction + 1e-4 * kld
@@ -635,7 +623,6 @@ class VAEController(ModelController):
     
     def step(self, loss: Tensor) -> None:
         self.scheduler.step()
-        self.lpips_mse_anneal += 1
 
 
     def name(self) -> str:
