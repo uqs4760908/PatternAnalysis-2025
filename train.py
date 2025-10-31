7bdbd8bbfd1ef194ad3d5893ca9ad138461ec5a0
s4760908 <s4760908@uq.edu.au>
2025-10-31 10:16:41 +1000

    Trainer

---
+++ train.py
@@ -0,0 +1,443 @@
+from torch.utils.data import DataLoader
+from torch.utils.data.distributed import DistributedSampler
+from config import VAEConfig
+from dataset import ANDIDataset, ImageListDataset
+from pathlib import Path
+from modules import VAE
+from torch import multiprocessing as mp
+from torch import distributed as dist
+from torch import GradScaler
+from torch.utils.tensorboard import SummaryWriter
+from torchvision import utils as vutils
+from tempfile import NamedTemporaryFile
+from torch import nn, Tensor
+from torch.nn import functional as F
+from torch.optim import Adam, Optimizer
+from dataclasses import dataclass
+import dataclasses
+import logging
+import colorlog
+import time
+import typing
+import abc
+import contextlib
+import torch
+import os
+
+
+# From https://github.com/CompVis/latent-diffusion/blob/main/models/ldm/celeba256/config.yaml
+# and https://github.com/Stability-AI/stablediffusion/blob/main/configs/stable-diffusion/v2-inference-v.yaml
+VAE_CONFIG = VAEConfig(
+    learn_rate=1.0e-06,
+    num_channels=(
+    1 * 128,
+    2 * 128,
+    4 * 128,
+    4 * 128
+    ),
+    latent_dim=4,
+    num_resnet_blocks=2,
+    attention_heads=1,
+    layer_norm_num_groups=32
+)
+
+
+def get_accelerator():
+    return torch.accelerator.current_accelerator() or torch.get_default_device()
+
+
+class PortableGradScaler:
+    def __init__(self):
+        # GradScaler is only availbale for cpu and cuda
+        accelerator = get_accelerator()
+        if accelerator.type in ("cuda", "cpu"):
+            self.scaler = GradScaler(accelerator.type)
+        else:
+            self.scaler = None
+
+
+    def scale(self, loss: Tensor):
+        if self.scaler is not None:
+            self.scaler.scale(loss).backward()
+
+
+    def update(self, optimiser: Optimizer):
+        if self.scaler is not None:
+            self.scaler.step(optimiser)
+            self.scaler.update()
+            optimiser.zero_grad(set_to_none=True)
+
+
+def pprint_stats(stats: typing.Any) -> list[str]:
+    lines: list[str] = []
+
+    for field in dataclasses.fields(stats):
+        value = getattr(stats, field.name)
+        if isinstance(value, Tensor) and value.numel() == 1:
+            value = value.item()
+
+        lines.append(f"{field.name}={value}")
+
+    return lines
+
+
+@dataclass(frozen=True)
+class DistributedParams:
+    rank: int
+
+
+RunModelFn = typing.Callable[[nn.Module, Tensor], typing.Any]
+
+
+@dataclass(frozen=True, slots=True)
+class RunModelParams:
+    model: nn.Module
+    device: torch.device
+    dataset: ImageListDataset
+    dist_params: typing.Optional[DistributedParams]
+
+
+    def is_master(self) -> bool:
+        return self.dist_params is None or self.dist_params.rank == 0
+
+
+RunModelLoop = typing.Callable[[RunModelParams], typing.Any]
+
+
+@dataclass(frozen=True)
+class RunStats:
+    loss: Tensor
+    generated_images: Tensor
+
+
+@dataclass(frozen=True)
+class EvalStats:
+    loss: Tensor
+    input_images: Tensor
+    generated_images: Tensor
+
+    
+def train_batch_wrapper(trainer: "TrainerBase", model: nn.Module, batch: Tensor):
+    return trainer.train_batch(model, batch)
+
+
+class TrainerBase(abc.ABC):
+    def __init__(self):
+        self.dataset = ANDIDataset(Path("./data"))
+
+        log = Path("train.log")
+        if log.exists():
+            os.truncate(log, 0)
+
+        console_handler = colorlog.StreamHandler()
+        console_handler.setFormatter(colorlog.ColoredFormatter("[%(log_color)s%(levelname)s:%(name)s%(reset)s]: %(message)s"))
+        file_handler = logging.FileHandler(log)
+        file_handler.setFormatter(logging.Formatter("[%(levelname)s:%(name)s]: %(message)s"))
+        self.logger = colorlog.getLogger(type(self).__name__)
+        self.logger.addHandler(console_handler)
+        self.logger.addHandler(file_handler)
+        self.logger.setLevel(logging.DEBUG)
+
+        #self.summary = SummaryWriter()
+
+
+    @contextlib.contextmanager
+    def setup(self, rank: int, world_size: int, file: str):
+        device = torch.accelerator.current_accelerator()
+        torch.set_float32_matmul_precision("high")
+
+        if torch.cuda.is_available():
+            torch.backends.cuda.matmul.allow_fp16_reduced_precision_reduction = True
+
+        if device is None:
+            return
+
+        store = dist.FileStore(file, world_size) # type:ignore
+        try:
+            backend = dist.get_default_backend_for_device(device)
+            dist.init_process_group(backend, store=store, rank=rank, world_size=world_size)
+            self.logger.info(f"Worker {os.getpid()}: rank={rank} backend={backend}")
+            yield None
+        finally:
+            dist.destroy_process_group()
+
+
+    @abc.abstractmethod
+    def model(self) -> nn.Module: ...
+
+
+    @abc.abstractmethod
+    def num_epochs(self) -> int: ...
+
+    
+    @abc.abstractmethod
+    def train_batch(self, model: nn.Module, batch: Tensor) -> RunStats: ...
+
+
+    @abc.abstractmethod
+    def eval_batch(self, model: nn.Module, batch: Tensor) -> RunStats: ...
+
+    
+    @abc.abstractmethod
+    def save_path(self) -> Path: ...
+
+
+    def batch_size(self) -> int:
+        return 4
+
+
+    def optimise_model(self, device: torch.device) -> nn.Module:
+        model = self.model().to(device)
+        model: nn.Module = model.to(memory_format=torch.channels_last) # type:ignore
+
+        if device.type == "cuda":
+            version = torch.cuda.get_device_capability(device)
+            properties = torch.cuda.get_device_properties(device)
+
+            # torch.compile requires CUDA 7.0+
+            # Also, compile() crashes on some device
+            # So far if device has more than 68 compute units compile() works
+            # See is_big_gpu() in torch/_inductor/utils.py
+            if version >= (7, 0) and properties.multi_processor_count >= 68:
+                model.compile()
+        else:
+            model.compile()
+        return model
+
+
+    def make_dataloader(self, params: RunModelParams):
+        if params.dist_params is not None:
+            sampler=DistributedSampler(params.dataset)
+        else:
+            sampler = None
+
+        loader = DataLoader(params.dataset, 
+                            batch_size=self.batch_size(), 
+                            shuffle=True, 
+                            sampler=sampler,
+                            num_workers=os.cpu_count() or 0)
+        return loader, sampler
+
+
+    def summary_writer(self, params: RunModelParams):
+        if params.is_master():
+            return SummaryWriter()
+        else:
+            return contextlib.nullcontext()
+
+
+    def train_loop(self, params: RunModelParams):
+        loader, sampler = self.make_dataloader(params)
+
+        with self.summary_writer(params) as summary:
+            for epoch in range(1, 2):
+                params.model.train()
+
+                epoch_start = time.time()
+
+                if sampler is not None:
+                    sampler.set_epoch(epoch)
+
+                avg_loss = torch.zeros(1, device=params.device, requires_grad=False)
+
+                for batch_idx, (batch, _) in enumerate(loader, start=1):
+                    batch: Tensor = batch.to(params.device)
+
+                    stats = self.train_batch(params.model, batch)
+                    avg_loss += stats.loss / len(loader)
+
+                    if (batch_idx % 50) == 0:
+                        self.logger.info(f"Training: epoch [{epoch}/{self.num_epochs()}] batch [{batch_idx}/{len(loader)}]")
+                        self.logger.info(f"\tLoss : {stats.loss.item()}")
+
+                if params.is_master():
+                    torch.save(params.model.state_dict(), self.save_path())
+
+                self.logger.info(f"Validating...")
+                eval_stats = self.eval_loop(RunModelParams(
+                    model=params.model,
+                    device=params.device,
+                    dataset=self.dataset.validation_dataset,
+                    dist_params=params.dist_params
+                ), tag="Validation")
+
+                if summary is not None:
+                    summary.add_scalar("Train loss", avg_loss, global_step=epoch)
+                    summary.add_scalar("Validation loss", eval_stats.loss, global_step=epoch)
+                    summary.add_image("Validation images(ground truth)", 
+                                       vutils.make_grid(eval_stats.input_images.detach().cpu()), 
+                                               global_step=epoch)
+                    summary.add_image("Validation images(generated)", 
+                                       vutils.make_grid(eval_stats.generated_images.detach().cpu()), 
+                                       global_step=epoch)
+
+                epoch_end = time.time()
+                self.logger.info(f"Epoch {epoch} done, took {epoch_end - epoch_start:2} seconds")
+
+            self.logger.info(f"Testing...")
+            test_stats = self.eval_loop(RunModelParams(
+                model=params.model,
+                device=params.device,
+                dataset=self.dataset.test_dataset,
+                dist_params=params.dist_params
+            ), tag="Test")
+
+            if summary is not None:
+                summary.add_scalar("Test loss", test_stats.loss)
+                summary.add_image("Test images(ground truth)", 
+                                   vutils.make_grid(test_stats.input_images.detach().cpu()))
+                summary.add_image("Test images(generated)",
+                                  vutils.make_grid(test_stats.generated_images.detach().cpu()))
+
+
+    @torch.inference_mode()
+    def eval_loop(self, params: RunModelParams, tag: str) -> EvalStats:
+        params.model.eval()
+
+        start = time.time()
+        loader, sampler = self.make_dataloader(params)
+
+        if sampler is not None:
+            sampler.set_epoch(1)
+
+        input_images = torch.empty(0)
+        generated_images = torch.empty(0)
+        avg_loss = torch.zeros(1, device=params.device, requires_grad=False)
+
+        for batch_idx, (batch, _) in enumerate(loader, start=1):
+            with torch.autocast(device_type=params.device.type):
+                batch: Tensor = batch.to(params.device)
+
+                stats = self.eval_batch(params.model, batch)
+            avg_loss += stats.loss / len(loader)
+
+            if generated_images.shape[0] == 0:
+                num_images = min(self.batch_size(), 8)
+                generated_images = stats.generated_images[:num_images]
+                input_images = batch[:num_images]
+
+            if (batch_idx % 50) == 0:
+                self.logger.info(f"{tag}: batch [{batch_idx}/{len(loader)}]")
+                self.logger.info(f"\tLoss {stats.loss.item()}")
+
+        end = time.time()
+
+        self.logger.info(f"{tag} done, took {end - start:2} seconds")
+        self.logger.info(f"\tAverage loss: {avg_loss.item()}")
+        return EvalStats(avg_loss, input_images, generated_images)
+
+
+    def run_model_worker(self, rank: int, world_size: int, file: str, fn: RunModelLoop, dataset: ImageListDataset):
+        with self.setup(rank, world_size, file):
+            accelerator = get_accelerator()
+            device = torch.device(f"{accelerator.type}:{rank}")
+            model = self.optimise_model(device)
+            model = nn.parallel.DistributedDataParallel(model)
+
+            fn(RunModelParams(
+                model=model,
+                device=device,
+                dataset=dataset,
+                dist_params=DistributedParams(rank)
+            ))
+
+
+    def run_model(self, fn: RunModelLoop, dataset: ImageListDataset):
+        nprocs = torch.accelerator.device_count()
+        device = get_accelerator()
+
+        self.logger.info(f"Using accelerator kind {device.type}")
+
+        if device.type in ("cuda", "xpu"):
+            self.logger.info(f"Found {nprocs} devices:")
+
+            for i in range(nprocs):
+                if device.type == "cuda":
+                    name = torch.cuda.get_device_name(i)
+                else:
+                    name = torch.xpu.get_device_name(i)
+
+                self.logger.info(f"\t[{i + 1}]: {name}")
+        else:
+            self.logger.info(f"Found {nprocs} devices")
+
+        # mps does not support DistributedDataParallel
+        if nprocs == 1 or device and device.type == "mps":
+            fn(RunModelParams(
+                model=self.optimise_model(device),
+                device=device,
+                dataset=dataset,
+                dist_params=None
+            ))
+        else:
+            # delete=False since FileStore deletes the file on close
+            with NamedTemporaryFile(delete=False) as file:
+                mp.spawn(self.run_model_worker, nprocs=nprocs, args=(nprocs, file.name, fn, dataset), join=True) # type:ignore
+
+    def train(self):
+        self.run_model(self.train_loop, self.dataset.train_dataset)
+
+
+    def done(self):
+        #self.summary.close()
+        pass
+
+
+@dataclass(frozen=True)
+class VAEStats:
+    loss: Tensor
+
+
+class VAETrainer(TrainerBase):
+    def __init__(self):
+        super().__init__()
+        self.vae = VAE(VAE_CONFIG, self.dataset.image_info)
+        self.optimiser = Adam(self.vae.parameters(), lr=VAE_CONFIG.learn_rate)
+        self.scaler = PortableGradScaler()
+
+
+    def num_epochs(self) -> int:
+        return 30
+
+
+    @staticmethod
+    def loss_fn(image: Tensor, generated: Tensor, mu: Tensor, logvar: Tensor) -> Tensor:
+        reconstruction: Tensor = F.mse_loss(image, generated, reduction="sum")
+        kld: Tensor = -0.5 * torch.sum(1 + logvar - mu.pow(2) - logvar.exp())
+
+        return reconstruction + kld
+
+
+    def save_path(self) -> Path:
+        return Path("vae.pth")
+
+
+    def train_batch(self, model: nn.Module, batch: Tensor) -> RunStats:
+        with torch.autocast(device_type=str(batch.device)):
+            generated, mu, logvar = model(batch)
+
+            loss = self.loss_fn(batch, generated, mu, logvar)
+
+        self.scaler.scale(loss)
+        nn.utils.clip_grad_norm_(self.vae.parameters(), max_norm=1)
+        self.scaler.update(self.optimiser)
+
+        return RunStats(loss=loss, generated_images=generated)
+
+    
+    def eval_batch(self, model: nn.Module, batch: Tensor) -> RunStats:
+        with torch.autocast(device_type=str(batch.device)):
+            generated, mu, logvar = model(batch)
+
+            loss = self.loss_fn(batch, generated, mu, logvar)
+            return RunStats(loss=loss, generated_images=generated)
+
+
+    def model(self) -> nn.Module:
+        return self.vae
+
+
+if __name__ == "__main__":
+    trainer = VAETrainer()
+    trainer.train()
+    trainer.done()
